01229eac42fa7982dfceb41326f7d0ec88adac77
Fisseha Teju <fishteju@gmail.com>
2021-09-14 23:48:39 +0800

    Add files via upload

---
+++ ui/UiRandomNumberGeneration.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TBDD.ui
+{
+    /// <summary>
+    /// Interaction logic for UiRandomNumberGeneration.xaml
+    /// </summary>
+    public partial class UiRandomNumberGeneration : Window
+    {
+        public UiRandomNumberGeneration()
+        {
+            InitializeComponent();
+        }
+    }
+}
